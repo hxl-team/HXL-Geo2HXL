cf2a8d93366e98dc7c22b71f66a08264e44c9ce8
CJ Hendrix <hendrix.cj@gmail.com>
2012-08-17 14:20:31 +0200

    Adds hxl:Country declaration.

--- geo2hxl.php
+++ geo2hxl.php
@@ -2,6 +2,7 @@
 //Geo2HXL.php: Reads csv of a single admin layer (converted to csv with WKT geom using ogr2ogr) to HXL triples.  
 
 //CHANGE LOG
+//Version 5 17/8/2012: Adds declaration for rdf:type hxl:country .
 //Version 4: Adds in logic to truncate to desired precision of WKT
 //Version 3: This version checks for whether or not the admin level being processed is level 0 (the national boundary).  It ignores the atLocation property in this case.
 //Version 2: This version has revisions to map to latest version of URI patterns and the Geolocation Standard.  It adds the writing of prefixes and the DataContainer.
@@ -16,20 +17,20 @@
 //See http://sites.google.com/site/ochaimwiki/cod-fod-guidance/administrative-boundaries for guidance on these fields
 //Also, configure the code that writes the prefixes at the beginning of the output file.  This is in the "add headers" section a bit further down.
 
-//Burkina Faso Admin 0 configuration
-//CSV header row: WKT,CNTRY_NAME,CNTRY_CODE
+//Burkina Faso Admin 1 configuration
+//CSV header row: WKT,CNTRY_NAME,CNTRY_CODE,ADM1_NAME,ADM1_COD_1
 $geom_element = 0 ; //which column contains the WKT geometry. First column = 0.
-$level_n_pcode_element = 2 ; //which column contains the pcode for the level that is being converted
-$level_n_minus_one_pcode_element = "ignored" ;  //which column contains the pcode for the admin unit one level above the level that is being converted. This is ignored if $n = 0 (See below).
-$n = 0 ; //base admin level being processed. Set to 0 if you are processing the national boundary.
-$featureName_element = 1 ; //which column contains the feature name
-$featureRefName_element = 1 ; //which column contains the feature ref name 
+$level_n_pcode_element = 4 ; //which column contains the pcode for the level that is being converted
+$level_n_minus_one_pcode_element = "2" ;  //which column contains the pcode for the admin unit one level above the level that is being converted. This is ignored if $n = 0 (See below).
+$n = 1 ; //base admin level being processed. Set to 0 if you are processing the national boundary.
+$featureName_element = 3 ; //which column contains the feature name
+$featureRefName_element = 3 ; //which column contains the feature ref name 
 $country_code = "bfa" ; //ISO 3 letter code for the country, lower case
 $precision = 7 ; //number of decimal places to which the WKT coordinates will be truncated. For Decimal Degrees, 7 yields approximately cm precision.  The default ogr2ogr output is 15 decimal places, about the radius of a hydrogen atom.
-$file_to_process = "bfa_admbnda_adm0_1m_salb.csv" ;
-$output_file_name = "bfa_admbnda_adm0_1m_salb.ttl" ;
+$file_to_process = "bfa_admbnda_adm1_1m_salb.csv" ;
+$output_file_name = "bfa_admbnda_adm1_1m_salb.ttl" ;
 //Metadata items
-$dcdate = "2012-08-13T11:16:00.0Z" ; //the date the file is created
+$dcdate = "2012-08-14T11:16:00.0Z" ; //the date the file is created
 $validityStart = "2012-07-24" ;  //Beginning date for which this dataset is the valid one.  This value is applied to the data container (named graph) which holds the data.  
 $validityEnd = "" ;  //Blank indicates that the dataset is currently valid.
 
@@ -85,7 +86,8 @@ $geom_uri = "geom" ;
 $hasSerialization_id = "hasSerialization" ;
 $wktLiteral_id = "wktLiteral";
 $Geometry_id = "Geometry" ;
-$DataContainer_id = "DataContainer";
+$DataContainer_id = "DataContainer" ;
+$Country_id = "Country" ;
 
 
 //------------------------START PROCESSING--------------------------------------------
@@ -120,7 +122,10 @@ while(!feof($csv_handle))
 	$admunit_uri = $base_uri . $oneup[$level_n_pcode_element] . ">" ;
 	
 	//create adminunit and its basic attributes
-	fwrite($output , $admunit_uri . " a " . $ns_uri . $AdminUnit_id . " .\n") ;
+	if ($n == 0)
+		{fwrite($output , $admunit_uri . " a " . $ns_uri . $Country_id . " .\n") ; }
+	else 
+		{fwrite($output , $admunit_uri . " a " . $ns_uri . $AdminUnit_id . " .\n") ; }
 	if ($n > 0)
 		{fwrite($output , $admunit_uri . " " . $ns_uri . $atLocation_id . " " . $base_uri . $oneup[$level_n_minus_one_pcode_element] . "> .\n") ;}
 	fwrite($output , $admunit_uri . " " . $ns_uri . $atLevel_id . " " . $base_uri . "adminlevel" . $n . "> .\n") ;
