7b57779a81065e192e619087c5ec2f0e94633673
CJ Hendrix <hendrix.cj@gmail.com>
2012-11-13 11:10:19 +0100

    Major revision: added pop places and missing data handling.

--- geo2hxl.php
+++ geo2hxl.php
@@ -2,6 +2,7 @@
 //Geo2HXL.php: Reads csv of a single admin layer (converted to csv with WKT geom using ogr2ogr) to HXL triples.  
 
 //CHANGE LOG
+//Version 7 09/11/2012: MAJOR REVISION - Adding logic to handle populated places
 //Version 6 07/11/2012: Modifications to the data container elements to reflect the ValidOn property.
 //Version 5 17/8/2012: Adds declaration for rdf:type hxl:country .
 //Version 4: Adds in logic to truncate to desired precision of WKT
@@ -12,27 +13,45 @@
 //Doesn't parse the date stamp (used for the data container name) to also get the dc:date literal.  dc:date is specified in the configuration.
 //Labels all data containers with the optional organization value set to "unocha".  A good future addition would be to make this configurable.
 //Some metadata elements for the datacontainer are missing (reported by, for example).  
+//Although it declares a feature to be at it's AdminUnitLevel, no further definition is given to these levels (the title of the level or the country to which it belongs).  
+//For the populated places classification, this script doesn't allow a delcaration of what a given class represents.  HXL has the ability to declare a "title" for a given class (ie: to indicate that "1stOrder" in HXL for a given country = "National Capital").  It would be good to add some logic to make these declartions.  The same is true for the admin levels, which can have titles, but this script doesn't handle declaring those.
 
 //-------------------CONFIGURATION-----------------------------------------------------
 //Delcare below which csv columns contain which data (first column = 0) and other needed info from user
 //See http://sites.google.com/site/ochaimwiki/cod-fod-guidance/administrative-boundaries for guidance on these fields
 //Also, configure the code that writes the prefixes at the beginning of the output file.  This is in the "add headers" section a bit further down.
 
-//Burkina Faso Admin 0 configuration
-//CSV header row: WKT,CNTRY_NAME,CNTRY_CODE
+//The sample configuration below is for Burkina Faso Populated Places
+//CSV header row: WKT,LAT,LONG,ADM1_NAME,ADM1_CODE,ADM2_NAME,ADM2_CODE,PCODE,NAME,CLASS
 $geom_element = 0 ; //which column contains the WKT geometry. First column = 0.
-$level_n_pcode_element = 2 ; //which column contains the pcode for the level that is being converted
-$level_n_minus_one_pcode_element = 999 ;  //which column contains the pcode for the admin unit one level above the level that is being converted. This is ignored if $n = 0 (See below).
-$n = 0 ; //base admin level being processed. Set to 0 if you are processing the national boundary.
-$featureName_element = 1 ; //which column contains the feature name
-$featureRefName_element = 1 ; //which column contains the feature ref name 
+$level_n_pcode_element = 7 ; //which column contains the pcode for the level that is being converted
+$level_n_minus_one_pcode_element = 6 ;  //which column contains the pcode for the admin unit one level above the level that is being converted. This is ignored if $n = 0 (See below).
+$n = 999 ; //base admin level being processed. Set to 0 if you are processing the national boundary. Set to 999 if you are processing the populated places layer.  Otherwise set to the admin level you are processing: 1, 2, 3, etc.
+$featureName_element = 8 ; //which column contains the feature name
+$featureRefName_element = 8 ; //which column contains the feature ref name.  Note that the script will automatically change accented characters if found, so this can be the same column as the $featureName_element.
+$popPlaceClass_element = 9 ; //for populated places, which column contains the class number.  Note that these must be numbers with the lower numbers representing higher status in the hierarchy.  See http://hxl.humanitarianresponse.info for details.
+
+//Populated Place Classes/Types/Categories Translation -------------------------------------------------------
+//The list of elements below describes a translation between any classification of populated places in your dataset to the HXL system.  This information is primarily used for cartographic symbolization of the populated place data.
+//If your data doesn't have information for a given class, just leave it blank (keep the 'ignored').  Otherwise, put the value in your dataset that would indicate the status of a populated place equal to what is in the [ ].   It is recommended that your biggest/most important code be given 1st order, your smallest/least important code be given 10th order, with other ranks being distributed between.  Blank orders are acceptable.  
+$pplClass['_1stOrder'] = '1' ;
+$pplClass['_2ndOrder'] = '2' ;
+$pplClass['_3rdOrder'] = '3' ;
+$pplClass['_4thOrder'] = 'ignored' ;
+$pplClass['_5thOrder'] = 'ignored' ;
+$pplClass['_6thOrder'] = '4' ;
+$pplClass['_7thOrder'] = 'ignored' ;
+$pplClass['_8thOrder'] = 'ignored' ;
+$pplClass['_9thOrder'] = 'ignored' ;
+$pplClass['_10thOrder'] = '0' ;
+//---------------------------------------
 $country_code = "bfa" ; //ISO 3 letter code for the country, lower case.  This becomes part of the URI for the features generated by the script.
 $precision = 7 ; //number of decimal places to which the WKT coordinates will be truncated. For Decimal Degrees, 7 yields approximately cm precision and is the recommended value.  The default ogr2ogr output is 15 decimal places, about the radius of a hydrogen atom.
-$file_to_process = "bfa_admbnda_adm0_1m_salb.csv" ;
-$output_file_name = "bfa_admbnda_adm0_1m_salb.ttl" ;
+$file_to_process = "BFA_pplp2_1m_NGA.csv" ;
+$output_file_name = "BFA_pplp2_1m_NGA.ttl" ;
 //Metadata items
 $dcdate = "2012-07-11T11:16:00.0Z" ; //the date the file is created
-$validon = "2012-07-24" ;  //Beginning date for which this dataset is the valid one.  This value is applied to the data container (i.e. named graph) which holds the data.  The end of the period of validity for this dataset is the first later ValidOn for a given feature.
+$validon = "2012-08-24" ;  //Beginning date for which this dataset is the valid one.  This value is applied to the data container (i.e. named graph) which holds the data.  The end of the period of validity for this dataset is the first later ValidOn for a given feature.
 
 //--------------FUNCTIONS--------------------------------------------------------------------------------------
 function truncate($precision, $current_geom)
@@ -84,8 +103,10 @@ $ns_uri = "hxl:" ;
 $geo_ns_uri = "geo:" ;
 $dc_ns_uri = "dc:" ;
 $AdminUnit_id = "AdminUnit" ;
+$PopulatedPlace_id = "PopulatedPlace" ;
 $atLocation_id = "atLocation" ;
 $atLevel_id = "atLevel" ;
+$inClass_id = "inClass" ;
 $pcode_id = "pcode" ;
 $featureName_id = "featureName" ;
 $featureRefName_id = "featureRefName" ;
@@ -119,30 +140,76 @@ fwrite ($output , $base_data_uri . "datacontainers" . "/" . "unocha/" . $timesta
 	{ fwrite ($output , $base_data_uri . "datacontainers" . "/" . "unocha/" . $timestamp . " " . $ns_uri . "validityEnd " . "\"" . $validityEnd . "\"" . " .\n") ;}*/
 
 fgetcsv($csv_handle,0,",") ; //reads and discards the first line
-
+$csvline = 2 ; //set line counter for error reporting
 while(!feof($csv_handle))
 	{
-	$oneup = fgetcsv($csv_handle,0,",") ;
+	$current = fgetcsv($csv_handle,0,",") ;
 	//test for blank line (usually last line at end)
-	if (count($oneup)==1)
+	if (count($current)==1)
 		{break;}
-		
-	$admunit_uri = $base_uri . $oneup[$level_n_pcode_element] . ">" ;
 	
-	//create adminunit and its basic attributes
-	if ($n == 0)
-		{fwrite($output , $admunit_uri . " a " . $ns_uri . $Country_id . " .\n") ; }
+	//check for missing data and write an error if found
+	if ($n == 999) //handles populated places (not admin units)
+		{$reject = "keep" ;
+		$testarray = array($current[$level_n_minus_one_pcode_element],$current[$level_n_pcode_element],$current[$featureName_element],$current[$featureRefName_element],$current[$popPlaceClass_element],$current[$geom_element]) ;
+		for ($i = 1; $i <= count($testarray); $i++)
+			{if ((empty($testarray[$i-1]) &&  $testarray[$i-1] != 0) || ($testarray[$i-1] == ""))
+				{$reject = "reject" ;}
+			}
+		}
+	elseif ($n == 0) //handles national boundaries (admin 0)
+		{$reject = FALSE ;
+		$testarray = array($current[$level_n_pcode_element],$current[$featureName_element],$current[$featureRefName_element],$current[$geom_element]) ;
+		for ($i = 1; $i <= count($testarray); $i++)
+			{if ($testarray[$i-1] == "")
+				{$reject = TRUE ;
+				break ;}
+			}
+		}
+	elseif ($n > 0 && $n < 999) //handles sub-national boundaries (admin >= 1)
+		{$reject = FALSE ;
+		$testarray = array($current[$level_n_minus_one_pcode_element],$current[$level_n_pcode_element],$current[$featureName_element],$current[$featureRefName_element],$current[$geom_element]) ;
+		for ($i = 1; $i <= count($testarray); $i++)
+			{if ($testarray[$i-1] == "")
+				{$reject = TRUE ;
+				break ;}
+			}
+		}
 	else 
-		{fwrite($output , $admunit_uri . " a " . $ns_uri . $AdminUnit_id . " .\n") ; }
-	if ($n > 0)
-		{fwrite($output , $admunit_uri . " " . $ns_uri . $atLocation_id . " " . $base_uri . $oneup[$level_n_minus_one_pcode_element] . "> .\n") ;}
-	fwrite($output , $admunit_uri . " " . $ns_uri . $atLevel_id . " " . $base_uri . "adminlevel" . $n . "> .\n") ;
-	fwrite($output , $admunit_uri . " " . $ns_uri . $pcode_id . " \"" . $oneup[$level_n_pcode_element] . "\" .\n") ;
-	fwrite($output , $admunit_uri . " " . $ns_uri . $featureName_id . " \"" . $oneup[$featureName_element] . "\" .\n") ;
-	fwrite($output , $admunit_uri . " " . $ns_uri . $featureRefName_id . " \"" . deaccent($oneup[$featureName_element]) . "\" .\n") ;
-	fwrite($output , $base_uri . $oneup[$level_n_pcode_element] . "/" . $geom_uri . ">" . " a " . $geo_ns_uri . $Geometry_id . " .\n") ;
-	fwrite($output , $admunit_uri . " " . $geo_ns_uri . $hasGeometry_id . " " . $base_uri . $oneup[$level_n_pcode_element] . "/" . $geom_uri . "> .\n") ;
-	fwrite($output , $base_uri . $oneup[$level_n_pcode_element] . "/" . $geom_uri . "> " . $geo_ns_uri . $hasSerialization_id . " " . "\"" . truncate($precision,$oneup[$geom_element]) . "\"^^" . $geo_ns_uri . $wktLiteral_id . " .\n") ;
+		{echo "Variable n is set to an illegal value which is > 999.  Exiting script.";
+		fclose($csv_handle) ;
+		fclose($output) ;
+		exit() ;
+		}
+	if ($reject == "reject")
+		{echo ("
+		Rejected line " . $csvline . " due to missing data") ;
+		}
+	else 	
+		{$admunit_uri = $base_uri . $current[$level_n_pcode_element] . ">" ;
+		
+		//create adminunit or populated place and its basic attributes
+		if ($n == 0) //handles admin 0 (national boundaries)
+			{fwrite($output , $admunit_uri . " a " . $ns_uri . $Country_id . " .\n") ; }  
+		elseif ($n > 0 && $n < 999) //handles sub-national admin boundaries 
+			{fwrite($output , $admunit_uri . " a " . $ns_uri . $AdminUnit_id . " .\n") ; 
+			fwrite($output , $admunit_uri . " " . $ns_uri . $atLevel_id . " " . $base_uri . "adminlevel" . $n . "> .\n") ;
+			fwrite($output , $admunit_uri . " " . $ns_uri . $atLocation_id . " " . $base_uri . $current[$level_n_minus_one_pcode_element] . "> .\n") ;}
+		elseif ($n == 999) //handles populated places
+			{fwrite($output , $admunit_uri . " a " . $ns_uri . $PopulatedPlace_id . " .\n") ; 
+			fwrite($output , $admunit_uri . " " . $ns_uri . $atLocation_id . " " . $base_uri . $current[$level_n_minus_one_pcode_element] . "> .\n") ;
+			//determine ppl class from pplClass array
+			$class = array_search($current[$popPlaceClass_element],$pplClass) ;
+			fwrite($output , $admunit_uri . " " . $ns_uri . $inClass_id . " " . $base_uri . "pplclass/" . $class . "> .\n") ;}
+		
+		fwrite($output , $admunit_uri . " " . $ns_uri . $pcode_id . " \"" . $current[$level_n_pcode_element] . "\" .\n") ;
+		fwrite($output , $admunit_uri . " " . $ns_uri . $featureName_id . " \"" . $current[$featureName_element] . "\" .\n") ;
+		fwrite($output , $admunit_uri . " " . $ns_uri . $featureRefName_id . " \"" . deaccent($current[$featureRefName_element]) . "\" .\n") ;
+		fwrite($output , $base_uri . $current[$level_n_pcode_element] . "/" . $geom_uri . ">" . " a " . $geo_ns_uri . $Geometry_id . " .\n") ;
+		fwrite($output , $admunit_uri . " " . $geo_ns_uri . $hasGeometry_id . " " . $base_uri . $current[$level_n_pcode_element] . "/" . $geom_uri . "> .\n") ;
+		fwrite($output , $base_uri . $current[$level_n_pcode_element] . "/" . $geom_uri . "> " . $geo_ns_uri . $hasSerialization_id . " " . "\"" . truncate($precision,$current[$geom_element]) . "\"^^" . $geo_ns_uri . $wktLiteral_id . " .\n") ;
+		}
+	$csvline ++;
 	}
  
  //close the files
